c817b138087a12bb1c2c35c6f1b7c09ef6568bad
Matt Ziegler <matthew_ziegler@intuit.com>
2014-07-21 13:58:18 -0700

    Added RDPViewController and moved main.storyboard to /views

---
+++ SpearnintIOS/RDPViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDPViewController.h
+//  SpearnintIOS
+//
+//  Created by Matthew Ziegler on 7/21/14.
+//  Copyright (c) 2014 Spearmint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDPViewController : UIViewController
+
+@end
