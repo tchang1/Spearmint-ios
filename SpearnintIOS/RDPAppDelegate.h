109b8182ee4efd82ed2853536e6dc0b61ba42d05
Corinne McElwain <cori_mcelwain@intuit.com>
2014-07-21 12:48:08 -0700

    Initial Commit

---
+++ SpearnintIOS/RDPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDPAppDelegate.h
+//  SpearnintIOS
+//
+//  Created by McElwain, Cori on 7/21/14.
+//  Copyright (c) 2014 Spearmint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpearnintIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpearnintIOS
+//
+//  Created by McElwain, Cori on 7/21/14.
+//  Copyright (c) 2014 Spearmint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDPAppDelegate class]));
+    }
+}
