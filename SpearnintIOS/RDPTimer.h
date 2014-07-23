8edbe9e48bd55d35b9726c0fcf403d99d9a7b559
Matt Ziegler <matthew_ziegler@intuit.com>
2014-07-23 14:38:47 -0700

    Completed Timers

--- SpearnintIOS/RDPTimer.h
+++ SpearnintIOS/RDPTimer.h
@@ -8,18 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void(^CompletionBlock)(void);
+
 @interface RDPTimer : NSObject
 
+/**
+ Creates a timer with no end time and no completion block. If you're thinking of using this,
+ you're probably doing something wrong.
+ */
 - (RDPTimer*) init;
 
+/**
+ Creates a timer with an end time with no completion block. This will be useful for things such
+ as interpolation where you want to get the progress over a set amount of time.
+ 
+ @param milliseconds : The number of milliseconds that the timer will be running for.
+ */
 - (RDPTimer*) initWithTime:(NSInteger)milliseconds;
 
-- (RDPTimer*) initWithTime:(NSInteger)milliseconds andCompletionBlock:(void (^)(void))block;
+/**
+ Creates a timer with a set amount of milliseconds and a completion block. This is useful for 
+ running a block of code after a set amount of time.
+ 
+ @param milliseconds : The number of milliseconds that the timer will be running for.
+ @param (block)block : The block of code to run once the timer has finished.
+ */
+- (RDPTimer*) initWithTime:(NSInteger)milliseconds andCompletionBlock:(CompletionBlock)block;
 
+/**
+ This progresses the timer forward a set number of milliseconds. This should only be called by
+ RDPTimerManager or another timer managing class.
+ 
+ @param milliseconds : The amount of milliseconds to progress the timer.
+ */
 - (void) tick:(NSInteger)milliseconds;
 
+/**
+ Manually sets a completion block for the timer.
+ 
+ @param (block)block : The block to call upon completion.
+ */
+- (void) setCompletionBlock:(CompletionBlock)block;
+
+/**
+ Gets the progress from 0.0 to 1.0 of the timer.
+ */
 - (NSNumber*) getProgress;
 
+/**
+ Returns if the timer has completed or not.
+ */
 - (BOOL) isComplete;
 
 @end
--- SpearnintIOS/RDPTimerManager.m
+++ SpearnintIOS/RDPTimerManager.m
@@ -10,50 +10,61 @@
 #import "RDPTimer.h"
 
 #define FPS 60
+#define kTolerance 0.001
 #define kQueueName "com.spearmint.updateQueue"
-
-typedef void (^UpdateBlock)(NSInteger);
-typedef void(^CompletionBlock)(void);
 
 static NSMutableArray* timers = nil;
 static NSMutableDictionary* blocks = nil;
-static NSInteger previousTime = 0;
+static double previousTime = 0;
 static BOOL needsUpdating = NO;
 static dispatch_queue_t updateQueue;
+static dispatch_source_t gcdTimer;
 
 @implementation RDPTimerManager
 
 +(void) update
 {
+    dispatch_source_cancel(gcdTimer);
+    gcdTimer = NULL;
     [RDPTimerManager validateNeedsUpdating];
     if (needsUpdating) {
-        NSInteger currentTime = [NSDate timeIntervalSinceReferenceDate];
-        NSInteger deltaTime = previousTime - currentTime;
+        double currentTime = CACurrentMediaTime() * 1000;
+        double deltaTime = (currentTime - previousTime);
         previousTime = currentTime;
         RDPTimer* timer;
         UpdateBlock updateBlock;
         NSInteger i = 0;
-        
+                
+        // "Ticks" all timers by the amount of milliseconds passed
         if (timers && [timers count] > 0) {
-            for(i = 0; i < [timers count]; i++) {
-                timer = [timers objectAtIndex:i];
+            // uses a copy of the timers array so that clients can remove timers in their completion block without
+            // destroying the for loop.
+            NSArray* timerCopies = [timers copy];
+            for(i = 0; i < [timerCopies count]; i++) {
+                timer = [timerCopies objectAtIndex:i];
                 [timer tick:deltaTime];
             }
         }
         
+        // Calls all update blocks and passes in the amount of milliseconds passed.
         if (blocks && [blocks count] > 0) {
-            for (NSString* blockName in blocks)
+            // uses a copy of the blocks dictionary so that clients can remove blocks in their update method without
+            // destroying the for loop.
+            NSDictionary* blocksCopy = [blocks copy];
+            for (NSString* blockName in blocksCopy)
             {
-                updateBlock = [blocks objectForKey:blockName];
+                updateBlock = [blocksCopy objectForKey:blockName];
                 if (updateBlock) {
                     updateBlock(deltaTime);
                 }
             }
         }
         
+        // Check to see if there are any timers or blocks that still need updating. If so,
+        // schedule this method to be called again in 1.0/FPS seconds.
         [RDPTimerManager validateNeedsUpdating];
         if (needsUpdating) {
-            
+            [RDPTimerManager callUpdateIn:(1.0/FPS)];
         }
         else {
             updateQueue = NULL;
@@ -61,9 +72,72 @@ static dispatch_queue_t updateQueue;
     }
 }
 
+/**
+ Check to see if there are any timers or blocks that need to be updated. If so,
+ mark the flag for needsUpdating. Otherwise, clear the update timer.
+ */
++(void) validateNeedsUpdating
+{
+    BOOL shouldUpdate = NO;
+    if (timers && [timers count] > 0) {
+        shouldUpdate = YES;
+    }
+    if (blocks && [blocks count] > 0) {
+        shouldUpdate = YES;
+    }
+    if (!shouldUpdate) {
+        gcdTimer = nil;
+    }
+    needsUpdating = shouldUpdate;
+}
+
+/**
+ Sets up the update queue and schedules the update block to run.
+ */
++(void) startUpdating
+{
+    if (!updateQueue) {
+        updateQueue = dispatch_queue_create(kQueueName , DISPATCH_QUEUE_SERIAL);
+    }
+    needsUpdating = YES;
+    previousTime = CACurrentMediaTime() * 1000;
+    [RDPTimerManager callUpdateIn:1.0/FPS];
+}
+
+/**
+ Schedules the update method to be called in x milliseconds.
+ */
++(void) callUpdateIn:(double)milliseconds
+{
+    if (!gcdTimer) {
+        gcdTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, updateQueue);
+        
+        if (gcdTimer) {
+            dispatch_source_set_timer(gcdTimer, dispatch_time(DISPATCH_TIME_NOW, milliseconds * NSEC_PER_SEC), milliseconds * NSEC_PER_SEC, (kTolerance * NSEC_PER_SEC));
+            dispatch_source_set_event_handler(gcdTimer,
+                                              ^(void){
+                                                  [RDPTimerManager update];
+                                              });
+            dispatch_resume(gcdTimer);
+        }
+    }
+}
+
 +(void) pauseFor:(NSInteger)milliseconds millisecondsThen: (void (^)(void))callbackBlock
 {
-    [RDPTimerManager createTimerWithTime:milliseconds andCompletionBlock:callbackBlock];
+    RDPTimer* timer = [RDPTimerManager createTimerWithTime:milliseconds];
+    __weak RDPTimer* weakTimer = timer;
+    
+    CompletionBlock newBlock = ^(void)
+    {
+        callbackBlock();
+        if (weakTimer) {
+            [RDPTimerManager clearTimer:weakTimer];
+        }
+    };
+    
+    NSLog(@"Setting new block");
+    [timer setCompletionBlock:newBlock];
 }
 
 +(RDPTimer*) createTimerWithTime:(NSInteger)milliseconds
@@ -79,7 +153,7 @@ static dispatch_queue_t updateQueue;
     }
     [RDPTimerManager startUpdating];
     [timers addObject:timer];
-    return nil;
+    return timer;
 }
 
 +(void) clearTimer:(RDPTimer*)timer
@@ -128,27 +202,6 @@ static dispatch_queue_t updateQueue;
 {
     [RDPTimerManager clearAllTimers];
     [RDPTimerManager clearAllUpdateBlocks];
-}
-
-+(void) validateNeedsUpdating
-{
-    BOOL shouldUpdate = NO;
-    if (timers && [timers count] > 0) {
-        shouldUpdate = YES;
-    }
-    if (blocks && [blocks count] > 0) {
-        shouldUpdate = YES;
-    }
-    needsUpdating = shouldUpdate;
-}
-
-+(void) startUpdating
-{
-    if (!updateQueue) {
-        updateQueue = dispatch_queue_create(kQueueName , DISPATCH_QUEUE_SERIAL);
-    }
-    needsUpdating = YES;
-    previousTime = [NSDate timeIntervalSinceReferenceDate];
 }
 
 @end
