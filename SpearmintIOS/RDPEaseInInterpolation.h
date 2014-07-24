0737e4d69074a9275388de21d4240fedd8310356
Matt Ziegler <matthew_ziegler@intuit.com>
2014-07-23 17:11:17 -0700

    Added all the interpolations
    Added all the interpolations

---
+++ SpearmintIOS/RDPEaseInInterpolation.h
@@ -0,0 +1,13 @@
+//
+//  RDPEaseInInterpolation.h
+//  SpearmintIOS
+//
+//  Created by Matthew Ziegler on 7/23/14.
+//  Copyright (c) 2014 Spearmint. All rights reserved.
+//
+
+#import "RDPInterpolation.h"
+
+@interface RDPEaseInInterpolation : RDPInterpolation
+
+@end
