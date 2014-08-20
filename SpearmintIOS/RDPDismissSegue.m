53093f6aba104ad04f4db41f487ff83c9a31ca4d
Tony Chang <ampedlemon@gmail.com>
2014-08-19 18:41:47 -0700

    dismiss segue

---
+++ SpearmintIOS/RDPDismissSegue.m
@@ -0,0 +1,13 @@
+//
+//  RDPDismissSegue.m
+//  SpearmintIOS
+//
+//  Created by Chang, Tony on 8/19/14.
+//  Copyright (c) 2014 Spearmint. All rights reserved.
+//
+
+#import "RDPDismissSegue.h"
+
+@implementation RDPDismissSegue
+
+@end
